4aa0454f9135fc82bc1330cdd648d3774137e679
pageinsec <36606674+pageinsec@users.noreply.github.com>
2025-05-16 07:57:31 -0500

    Adding shortcut layer

--- keyboards/lily58/rev1/keymaps/colemak_win_mac/keymap.c
+++ keyboards/lily58/rev1/keymaps/colemak_win_mac/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 
+
 enum layer_number {
   _COLEMAKW = 0,
   _COLEMAKM,
@@ -26,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_COLEMAKM] = LAYOUT(
-  KC_EQL,   KC_1,           KC_2,           KC_3,           KC_4,             KC_5,                                                 KC_6,             KC_7,             KC_8,           KC_9,           KC_0,            KC_MINS,
-  KC_TAB,   KC_Q,           KC_W,           KC_F,           KC_P,             KC_G,                                                 KC_J,             KC_L,             KC_U,           KC_Y,           KC_SCLN,         KC_GRV,
-  KC_BSPC,  LSFT_T(KC_A),   LCTL_T(KC_R),   LGUI_T(KC_S),   LALT_T(KC_T),     KC_D,                                                 KC_H,             RALT_T(KC_N),     RGUI_T(KC_E),   RCTL_T(KC_I),   RSFT_T(KC_O),    KC_QUOT,
-  KC_GRAVE,   LGUI_T(KC_Z),   KC_X,           KC_C,           LSFT_T(KC_V),     KC_B,             LCA_T(KC_MINUS),   MEH_T(KC_MINUS), KC_K,             RSFT_T(KC_M),    KC_COMM,        KC_DOT,         RGUI_T(KC_SLSH), CW_TOGG,
-                                            KC_DEL,         KC_LEFT,          LALT_T(KC_RIGHT), LT(2, KC_SPC),                      LT(3, KC_ENT),    RALT_T(KC_DOWN),  KC_UP,          KC_ESC
+  KC_EQL,   KC_1,           KC_2,           KC_3,           KC_4,             KC_5,                                                     KC_6,               KC_7,           KC_8,           KC_9,           KC_0,            KC_MINS,
+  KC_TAB,   KC_Q,           KC_W,           KC_F,           KC_P,             KC_G,                                                     KC_J,               KC_L,           KC_U,           KC_Y,           KC_SCLN,         KC_GRV,
+  KC_BSPC,  LSFT_T(KC_A),   LCTL_T(KC_R),   LGUI_T(KC_S),   LALT_T(KC_T),     KC_D,                                                     KC_H,               RALT_T(KC_N),   RGUI_T(KC_E),   RCTL_T(KC_I),   RSFT_T(KC_O),    LT(4, KC_QUOT),
+  KC_GRAVE, LGUI_T(KC_Z),   KC_X,           KC_C,           LSFT_T(KC_V),     LT(4, KC_B),          LCA_T(KC_MINUS),   MEH_T(KC_MINUS), KC_K,               RSFT_T(KC_M),   KC_COMM,        KC_DOT,         RGUI_T(KC_SLSH), CW_TOGG,
+                                            KC_DEL,         KC_LEFT,          LALT_T(KC_RIGHT),     LT(2, KC_SPC),     LT(3, KC_ENT),   RALT_T(KC_DOWN),    KC_UP,          KC_ESC
 ),
 /* LOWER
  * Number and symbol layer
@@ -43,28 +44,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_TRNS, KC_TRNS, KC_LCTL,       KC_TRNS,     KC_TRNS,     KC_TRNS, KC_P0, KC_P0
 ),
 /* RAISE
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
+ * Mouse and navigation layer
  */
-
 [_RAISE] = LAYOUT(
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                          TO(0),    TO(1),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_NO,    MS_UP,    KC_NO,    MS_WHLU,                          KC_NO,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS,
-  KC_TRNS,  KC_LGUI,  MS_LEFT,  MS_DOWN,  MS_RGHT,  MS_WHLD,                          KC_NO,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
-  KC_TRNS,  KC_LCTL,  KC_LSFT,  MS_BTN1,  MS_BTN2,  KC_LEFT_SHIFT,  KC_TRNS, KC_TRNS, KC_PLUS,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-                                KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                          TO(0),    TO(1),          KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_NO,    MS_UP,    KC_NO,    MS_WHLU,                          KC_NO,    KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,             KC_TRNS,
+  KC_TRNS,  KC_LGUI,  MS_LEFT,  MS_DOWN,  MS_RGHT,  MS_WHLD,                          KC_NO,    KC_LEFT,        KC_DOWN,         KC_UP,         KC_RGHT,            KC_NO,
+  KC_TRNS,  KC_LCTL,  KC_LSFT,  MS_BTN1,  MS_BTN2,  KC_LEFT_SHIFT,  KC_TRNS, KC_TRNS, KC_PLUS,  LCTRL(KC_LEFT), LALT(KC_LEFT),  LALT(KC_RIGHT), LCTRL(KC_RIGHT),    KC_BSLS,
+                                KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,        KC_TRNS
 ),
-/* ADJUST
+
+[_NAV] = LAYOUT(
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,        KC_TRNS,                    TO(0),          TO(1),          KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_NO,    C(KC_LEFT),     C(KC_RIGHT),    MS_WHLU,                    KC_NO,          KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,             KC_TRNS,
+  KC_TRNS,  KC_LALT,  KC_LCTRL, KC_LSFT,        KC_TAB,         MS_WHLD,                    KC_NO,          KC_LEFT,        KC_DOWN,         KC_UP,         KC_RGHT,            KC_NO,
+  KC_TRNS,  KC_LGUI,  KC_LSFT,  RCS(KC_TAB),    LCTRL(KC_TAB),  KC_TRNS,  KC_TRNS, KC_TRNS, LGUI(KC_LEFT),  LCTRL(KC_LEFT), LALT(KC_LEFT),  LALT(KC_RIGHT), LCTRL(KC_RIGHT),    LGUI(KC_RIGHT),
+                                KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,        KC_TRNS
+),
+/* ADJUST - currently unused
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
