a605c8e8f523606ed1d9b48d48c093e1e5e68beb
pageinsec <36606674+pageinsec@users.noreply.github.com>
2025-08-06 05:44:35 -0500

    Navigation tweaks

--- keyboards/lily58/rev1/keymaps/colemak_win_mac/keymap.c
+++ keyboards/lily58/rev1/keymaps/colemak_win_mac/keymap.c
@@ -51,19 +51,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Mouse and navigation layer
  */
 [_RAISE] = LAYOUT(
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                          TO(0),    TO(1),          KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_NO,    MS_UP,    KC_NO,    MS_WHLU,                          C(G(A(KC_LEFT))),    C(A(KC_LEFT)),        C(A(KC_UP)),        C(A(KC_DOWN)),        C(A(KC_RIGHT)),             C(G(A(KC_RIGHT))),
-  KC_TRNS,  KC_LGUI,  MS_LEFT,  MS_DOWN,  MS_RGHT,  MS_WHLD,                          KC_NO,    KC_LEFT,        KC_DOWN,         KC_UP,         KC_RGHT,            KC_NO,
-  KC_TRNS,  KC_LCTL,  KC_LSFT,  MS_BTN1,  MS_BTN2,  KC_LEFT_SHIFT,  KC_TRNS, KC_TRNS, C(KC_LEFT),  G(KC_LEFT), LALT(KC_LEFT),  LALT(KC_RIGHT), G(KC_RIGHT),    C(KC_RIGHT),
-                                KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,        KC_TRNS
+  KC_TRNS,  KC_TRNS,  KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,                    TO(0),            TO(1),          KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+  KC_TRNS,  KC_TRNS,  RCS(KC_TAB),  MS_UP,    C(KC_TAB),    MS_WHLU,                    C(G(A(KC_LEFT))), C(A(KC_LEFT)),  C(A(KC_UP)),    C(A(KC_DOWN)),  C(A(KC_RIGHT)), C(G(A(KC_RIGHT))),
+  KC_TRNS,  KC_LSFT,  MS_LEFT,      MS_DOWN,  MS_RGHT,      MS_WHLD,                    KC_NO,            KC_LEFT,        KC_DOWN,        KC_UP,          KC_RGHT,        KC_NO,
+  KC_TRNS,  KC_LCTL,  KC_LSFT,      MS_BTN1,  MS_BTN2,      KC_LSFT,  KC_TRNS, KC_TRNS, C(KC_LEFT),       G(KC_LEFT),     LALT(KC_LEFT),  LALT(KC_RIGHT), G(KC_RIGHT),    C(KC_RIGHT),
+                      KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,                    KC_TRNS,          KC_TRNS,        KC_TRNS,        KC_TRNS
 ),
 
 [_NAV] = LAYOUT(
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,        KC_TRNS,                    TO(0),      TO(1),          KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,
-  KC_TRNS,  KC_TRNS,  KC_NO,    C(KC_LEFT),     C(KC_RIGHT),    MS_WHLU,                    KC_NO,      KC_HOME,        KC_PGDN,        KC_PGUP,        KC_END,             KC_TRNS,
-  KC_TRNS,  KC_LALT,  KC_LCTL, KC_LSFT,        KC_TAB,         MS_WHLD,                    KC_NO,      KC_LEFT,        KC_DOWN,         KC_UP,         KC_RGHT,            KC_NO,
-  KC_TRNS,  KC_LGUI,  KC_LSFT,  RCS(KC_TAB),    C(KC_TAB),  KC_TRNS,  KC_TRNS, KC_TRNS, G(KC_LEFT),  G(KC_LEFT), LALT(KC_LEFT),  LALT(KC_RIGHT), G(KC_RIGHT),    C(KC_RIGHT),
-                                KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,        KC_TRNS,                    TO(0),      TO(1),      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+  KC_TRNS,  KC_TRNS,  KC_NO,    C(KC_LEFT),     C(KC_RIGHT),    MS_WHLU,                    KC_NO,      KC_HOME,    KC_PGDN,        KC_PGUP,        KC_END,         KC_TRNS,
+  KC_TRNS,  KC_LALT,  KC_LCTL,  KC_LSFT,        KC_TAB,         MS_WHLD,                    KC_NO,      KC_LEFT,    KC_DOWN,        KC_UP,          KC_RGHT,        KC_NO,
+  KC_TRNS,  KC_LGUI,  KC_LSFT,  RCS(KC_TAB),    C(KC_TAB),      KC_TRNS,  KC_TRNS, KC_TRNS, C(KC_LEFT), G(KC_LEFT), LALT(KC_LEFT),  LALT(KC_RIGHT), G(KC_RIGHT),    C(KC_RIGHT),
+                                KC_TRNS,        KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS
 ),
 /* ADJUST - currently unused
  * ,-----------------------------------------.                    ,-----------------------------------------.
